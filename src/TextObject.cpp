7b29ffbe41868eccaee1c35982bec962d811b801
Pordrack <50073154+Pordrack@users.noreply.github.com>
2023-10-12 10:05:53 +0200

    WIPText

--- src/TextObject.cpp
+++ src/TextObject.cpp
@@ -1,1 +1,32 @@
 #include "TextObject.h"
+
+void TextObject::SetText(std::string text)
+{
+	_text = text;
+	_children.clear();
+	for (int i = 0; i < _text.size(); i++)
+	{
+		char c = _text[i];
+		if (c == ' ')
+			continue;
+		
+	}
+}
+
+void TextObject::SetColor(int color)
+{
+	_color = color;
+	SetText(_text);
+}
+
+void TextObject::SetAlpha(float alpha)
+{
+	_alpha = alpha;
+	SetText(_text);
+}
+
+void TextObject::SetFontWidth(int size)
+{
+	_fontWidth = size;
+	SetText(_text);
+}
