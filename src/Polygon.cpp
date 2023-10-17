71feec6d58d5ea3300afa92a54f5f8fc3699dc42
Pordrack <50073154+Pordrack@users.noreply.github.com>
2023-10-17 14:00:47 +0200

    GetPolygons method

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -7,84 +7,87 @@
 
 namespace core {
 
-Vector2 Polygon::GetBoundingBox()
-{
-	ComputePoints();
-	float minX = INFINITY;
-	float minY = INFINITY;
-	float maxX = -INFINITY;
-	float maxY = -INFINITY;
+	std::vector<std::shared_ptr<Polygon>> Polygon::_polygons = {};
 
-	for (auto point : _computedPoints)
+	Vector2 Polygon::GetBoundingBox()
 	{
-		minX = std::min(minX, point._x);
-		minY = std::min(minY, point._y);
-		maxX = std::max(maxX, point._x);
-		maxY = std::max(maxY, point._y);
+		ComputePoints();
+		float minX = INFINITY;
+		float minY = INFINITY;
+		float maxX = -INFINITY;
+		float maxY = -INFINITY;
+
+		for (auto point : _computedPoints)
+		{
+			minX = std::min(minX, point._x);
+			minY = std::min(minY, point._y);
+			maxX = std::max(maxX, point._x);
+			maxY = std::max(maxY, point._y);
+		}
+
+		return Vector2(maxX - minX, maxY - minY);
 	}
 
-	return Vector2(maxX - minX, maxY - minY);
-}
-
-float Polygon::GetDiameter()
-{
-	Vector2 boundingBox = GetBoundingBox();
-	float maxDiamater = boundingBox.Magnitude();
-	for (auto point : _computedPoints)
+	float Polygon::GetDiameter()
 	{
-		maxDiamater = std::max(maxDiamater, (GetLocalPosition() + point).Magnitude());
+		Vector2 boundingBox = GetBoundingBox();
+		float maxDiamater = boundingBox.Magnitude();
+		for (auto point : _computedPoints)
+		{
+			maxDiamater = std::max(maxDiamater, (GetLocalPosition() + point).Magnitude());
+		}
+
+		return maxDiamater;
 	}
 
-	return maxDiamater;
-}
-
-void Polygon::Draw()
-{
-	GameObject::Draw();
-
-	if (_computedPoints.size() != _points.size())
+	void Polygon::Draw()
 	{
+		GameObject::Draw();
+
+		if (_computedPoints.size() != _points.size())
+		{
+			ComputePoints();
+		}
+
+		for (size_t i = 0; i < _computedPoints.size() - (_isClosed ? 0 : 1); ++i)
+		{
+			Vector2 currentPoint = GetWorldPosition() + (_computedPoints[i]);
+			Vector2 nextPoint = GetWorldPosition() + (_computedPoints[(i < _computedPoints.size() - 1) ? i + 1 : 0]);
+
+			Drawable::DrawLine(currentPoint, nextPoint, _color);
+		}
+
+		auto position = GetWorldPosition();
+	}
+
+	void Polygon::RotateToRadians(float targetAngle)
+	{
+		GameObject::RotateToRadians(targetAngle);
 		ComputePoints();
 	}
 
-	for (size_t i = 0; i < _computedPoints.size() - (_isClosed ? 0 : 1); ++i)
+	void Polygon::SetLocalScale(Vector2 targetScale)
 	{
-		Vector2 currentPoint = GetWorldPosition() + (_computedPoints[i]);
-		Vector2 nextPoint = GetWorldPosition() + (_computedPoints[(i < _computedPoints.size() - 1) ? i + 1 : 0]);
-
-		Drawable::DrawLine(currentPoint, nextPoint, _color);
+		GameObject::SetLocalScale(targetScale);
+		ComputePoints();
 	}
 
-	auto position = GetWorldPosition();
-}
-
-void Polygon::RotateToRadians(float targetAngle)
-{
-	GameObject::RotateToRadians(targetAngle);
-	ComputePoints();
-}
-
-void Polygon::SetLocalScale(Vector2 targetScale)
-{
-	GameObject::SetLocalScale(targetScale);
-	ComputePoints();
-}
-
-void Polygon::OnCreationOver()
-{
-	GameObject::OnCreationOver();
-	ComputePoints();
-}
-
-void Polygon::ComputePoints()
-{
-	if (_computedPoints.size() != _points.size())
-		_computedPoints.resize(_points.size());
-
-	for (size_t i = 0; i < _points.size(); ++i)
+	void Polygon::OnCreationOver()
 	{
-		_computedPoints[i] = (GetWorldScale() * _points[i]).RotateByRadians(GetWorldRotationInRadians());
+		_polygons.push_back(std::dynamic_pointer_cast<Polygon>(shared_from_this()));
+		GameObject::OnCreationOver();
+		ComputePoints();
 	}
-}
+
+	void Polygon::ComputePoints()
+	{
+		if (_computedPoints.size() != _points.size())
+			_computedPoints.resize(_points.size());
+
+		for (size_t i = 0; i < _points.size(); ++i)
+		{
+			_computedPoints[i] = (GetWorldScale() * _points[i]).RotateByRadians(GetWorldRotationInRadians());
+		}
+	}
 
 }
--- src/Polygon.h
+++ src/Polygon.h
@@ -6,41 +6,58 @@
 
 namespace core {
 
-class Polygon :
-	public GameObject
-{
-public:
-	std::vector<Vector2> _points; //Coordinates are relatives to the position of the GameObject
-	float _alpha;
-	bool _isClosed;
-
-	virtual Vector2 GetBoundingBox() override;
-	virtual float GetDiameter() override;
-
-	virtual void Draw() override;
-	void RotateToRadians(float targetAngle);
-
-    virtual void SetLocalScale(Vector2 targetScale) override;
-
-	virtual void Update() override {}
-
-	Polygon(): _alpha(1), _isClosed(true) {
-		_color = WHITE;
-	};
-
-	Polygon(std::vector<Vector2> points, bool isClosed, Color color = WHITE, float alpha = 1, Vector2 localPosition = Vector2(0, 0), Vector2 localScale = Vector2(1, 1), float localRotation=0) :_points(points), 
-		_alpha(alpha), _isClosed(isClosed), _computedPoints(_points)
+	class Polygon :
+		public GameObject
 	{
-		_color = color;
-		_localPosition = localPosition;
-		_localScale = localScale;
-		_localRotation = localRotation;
-	};
+	public:
+		std::vector<Vector2> _points; //Coordinates are relatives to the position of the GameObject
+		float _alpha;
+		bool _isClosed;
 
-	void OnCreationOver() override;
-protected:
-    std::vector<Vector2> _computedPoints;
-    void ComputePoints();
-};
+		virtual Vector2 GetBoundingBox() override;
+		virtual float GetDiameter() override;
+
+		virtual void Draw() override;
+		void RotateToRadians(float targetAngle);
+
+		virtual void SetLocalScale(Vector2 targetScale) override;
+
+		void OnCreationOver() override;
+
+		virtual void Update() override {}
+
+		Polygon() : _alpha(1), _isClosed(true) {
+			_color = WHITE;
+		};
+
+		Polygon(std::vector<Vector2> points, bool isClosed, Color color = WHITE, float alpha = 1, Vector2 localPosition = Vector2(0, 0), Vector2 localScale = Vector2(1, 1), float localRotation = 0) :_points(points),
+			_alpha(alpha), _isClosed(isClosed), _computedPoints(_points)
+		{
+			_color = color;
+			_localPosition = localPosition;
+			_localScale = localScale;
+			_localRotation = localRotation;
+		};
+
+		void virtual Destroy() override {
+			GameObject::Destroy();
+			//iterate over polygons to find a shared_ptr to this one and erase it
+			for (auto itr = _polygons.begin(); itr != _polygons.end(); ++itr)
+			{
+				if ((*itr) != shared_from_this()) continue;
+				_polygons.erase(itr);
+				break;
+			}
+		}
+
+		static std::vector<std::shared_ptr<Polygon>> GetPolygons() {
+			return _polygons;
+		};
+
+	protected:
+		std::vector<Vector2> _computedPoints;
+		void ComputePoints();
+		static std::vector<std::shared_ptr<Polygon>> _polygons;
+	};
 
 }
